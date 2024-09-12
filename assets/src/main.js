6c3ebeb279581836663d5ad394ff177ef38cdf19
aurelie nogueira <aurelienog@gmail.com>
2024-09-12 14:01:46 +0200

    music fixed

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,10 +1,10 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext("2d");
 const h2 = document.getElementById('blink')
-
+const audio = document.createElement("audio");
 
 document.onclick = () => {
-  const audio = document.createElement("audio");
+  
   audio.preload = "auto";
   audio.src = ("assets/src/music/mainMusic.mp3");
   audio.volume = 0.1;
@@ -20,6 +20,7 @@ document.onkeydown = () => {
   canvas.style.display = 'block';
   
   game.start();
+  audio.pause()
 }
 
 
